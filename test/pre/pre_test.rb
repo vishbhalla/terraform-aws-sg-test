a1ac82731ca55e35ffaa08154a7fbf9f3af48bac
Vish <vishal.m.bhalla@gmail.com>
2019-10-08 10:30:17 +0100

    Update pre_test.rb

--- test/pre/pre_test.rb
+++ test/pre/pre_test.rb
@@ -1,6 +1,5 @@
 title "Pre deployment tests"
 
-#describe aws_vpc(vpc_id: 'vpc-12def674') do
-describe aws_vpc(vpc_id: 'vpc-12def671') do
+describe aws_vpc(vpc_id: 'vpc-12def674') do
   it { should exist }
 end
